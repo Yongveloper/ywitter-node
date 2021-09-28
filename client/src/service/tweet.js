a076545a93401ca75aba93b5eb75e041c17bcca0
Yongveloper <aaa9015@naver.com>
2021-09-28 21:42:27 +0900

    솔루션 보기 전 코드

--- client/src/service/tweet.js
+++ client/src/service/tweet.js
@@ -1,50 +1,58 @@
 export default class TweetService {
-  tweets = [
-    {
-      id: 1,
-      text: '드림코딩에서 강의 들으면 너무 좋으다',
-      createdAt: '2021-05-09T04:20:57.000Z',
-      name: 'Bob',
-      username: 'bob',
-      url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-    },
-  ];
+  // tweets = [
+  //   {
+  //     id: 1,
+  //     text: '드림코딩에서 강의 들으면 너무 좋으다',
+  //     createdAt: '2021-05-09T04:20:57.000Z',
+  //     name: 'Bob',
+  //     username: 'bob',
+  //     url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
+  //   },
+  // ];
 
   async getTweets(username) {
-    // return username
-    //   ? this.tweets.filter((tweet) => tweet.username === username)
-    //   : this.tweets;
-    if (username) {
-      return this.tweets.filter((tweet) => tweet.username === username);
-    } else {
-      const response = await fetch('http://localhost:8080/tweets');
-      const data = await response.json();
-      return data;
-    }
+    const response = username
+      ? await fetch(`http://localhost:8080/tweets?username=${username}`)
+      : await fetch('http://localhost:8080/tweets');
+    const tweets = await response.json();
+    return tweets;
   }
 
   async postTweet(text) {
-    const tweet = {
-      id: Date.now(),
-      createdAt: new Date(),
-      name: 'Ellie',
-      username: 'ellie',
-      text,
-    };
-    this.tweets.push(tweet);
+    const response = await fetch('http://localhost:8080/tweets', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: 'yong', username: 'yong', text }),
+    });
+    const tweet = await response.json();
     return tweet;
   }
 
   async deleteTweet(tweetId) {
-    this.tweets = this.tweets.filter((tweet) => tweet.id !== tweetId);
+    await fetch(`http://localhost:8080/tweets/${tweetId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ tweetId }),
+    });
   }
 
   async updateTweet(tweetId, text) {
-    const tweet = this.tweets.find((tweet) => tweet.id === tweetId);
-    if (!tweet) {
-      throw new Error('tweet not found!');
+    try {
+      const response = await fetch(`http://localhost:8080/tweets/${tweetId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text }),
+      });
+      const tweet = await response.json();
+      return tweet;
+    } catch (error) {
+      console.error('해당 글을 찾을 수 없습니다.');
     }
-    tweet.text = text;
-    return tweet;
   }
 }
--- server/router/tweets.js
+++ server/router/tweets.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-const tweets = [
+let tweets = [
   {
     id: 1,
     text: '테스트입니다',
@@ -19,10 +19,63 @@ const tweets = [
     username: 'yong',
     url: '',
   },
+  {
+    id: 3,
+    text: '밥밥',
+    createdAt: '2021-05-20T04:20:57.000Z',
+    name: 'bob',
+    username: 'bob',
+    url: '',
+  },
 ];
 
 router.get('/', (req, res) => {
-  res.status(200).json(tweets);
+  const { username } = req.query;
+  if (username) {
+    const userTweets = tweets.filter((tweet) => tweet.username === username);
+    res.status(200).json(userTweets);
+  } else {
+    res.status(200).json(tweets);
+  }
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const specificTweets = tweets.find((tweet) => tweet.id === Number(id));
+  if (specificTweets) {
+    res.status(200).json(specificTweets);
+  } else {
+    res.status(404).send();
+  }
+});
+
+router.post('/', (req, res) => {
+  const { text, username, name } = req.body;
+  const newTweet = {
+    id: Date.now(),
+    name,
+    username,
+    createdAt: new Date(),
+    text,
+    url: '',
+  };
+  tweets.push(newTweet);
+  res.status(201).json(newTweet);
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const { text } = req.body;
+  const newTweet = tweets.find((tweet) => tweet.id === Number(id));
+  if (!newTweet) return res.status(404).send();
+  newTweet.text = text;
+  res.status(200).json(newTweet);
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  tweets = tweets.filter((tweet) => tweet.id !== Number(id));
+  res.status(204).send();
 });
 
 export default router;
