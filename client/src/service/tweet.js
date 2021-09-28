54db65b24d1206a0743f13a5323cb5c48ec89451
Yongveloper <aaa9015@naver.com>
2021-09-28 23:05:57 +0900

    클라이언트 솔루션 코드 에러 처리 필요했음, 약간의 코드 개선

--- client/src/service/tweet.js
+++ client/src/service/tweet.js
@@ -1,58 +1,57 @@
 export default class TweetService {
-  // tweets = [
-  //   {
-  //     id: 1,
-  //     text: '드림코딩에서 강의 들으면 너무 좋으다',
-  //     createdAt: '2021-05-09T04:20:57.000Z',
-  //     name: 'Bob',
-  //     username: 'bob',
-  //     url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-  //   },
-  // ];
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+  }
 
   async getTweets(username) {
-    const response = username
-      ? await fetch(`http://localhost:8080/tweets?username=${username}`)
-      : await fetch('http://localhost:8080/tweets');
-    const tweets = await response.json();
-    return tweets;
+    const query = username ? `?username=${username}` : '';
+    const response = await fetch(`${this.baseURL}/tweets${query}`);
+    const data = await response.json();
+    if (response.status !== 200) {
+      throw new Error(data.message);
+    }
+    return data;
   }
 
   async postTweet(text) {
-    const response = await fetch('http://localhost:8080/tweets', {
+    const response = await fetch(`${this.baseURL}/tweets`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ name: 'yong', username: 'yong', text }),
     });
-    const tweet = await response.json();
-    return tweet;
+    const data = await response.json();
+    if (response.status !== 201) {
+      throw new Error(data.message);
+    }
+    return data;
   }
 
   async deleteTweet(tweetId) {
-    await fetch(`http://localhost:8080/tweets/${tweetId}`, {
+    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ tweetId }),
     });
+    if (response.status !== 204) {
+      throw new Error();
+    }
   }
 
   async updateTweet(tweetId, text) {
-    try {
-      const response = await fetch(`http://localhost:8080/tweets/${tweetId}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ text }),
-      });
-      const tweet = await response.json();
-      return tweet;
-    } catch (error) {
-      console.error('해당 글을 찾을 수 없습니다.');
+    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text }),
+    });
+    const data = await response.json();
+    if (response.status !== 200) {
+      throw new Error(data.message);
     }
+    return data;
   }
 }
