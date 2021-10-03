6a4f61c55ee9224748c49415ccafd5f14eaa0d1f
Yongveloper <aaa9015@naver.com>
2021-10-03 21:52:02 +0900

    MVC 패턴 리팩토링 client

--- server/controller/tweet.js
+++ server/controller/tweet.js
@@ -1,16 +1,16 @@
 import * as tweetRepository from '../data/tweet.js';
 
-export function getTweets(req, res) {
+export async function getTweets(req, res) {
   const { username } = req.query;
-  const data = username
+  const data = await (username
     ? tweetRepository.getAllByUsername(username)
-    : tweetRepository.getAll();
+    : tweetRepository.getAll());
   res.status(200).json(data);
 }
 
-export function getTweet(req, res) {
+export async function getTweet(req, res) {
   const { id } = req.params;
-  const tweet = tweetRepository.getById(id);
+  const tweet = await tweetRepository.getById(id);
   if (tweet) {
     res.status(200).json(tweet);
   } else {
@@ -18,16 +18,16 @@ export function getTweet(req, res) {
   }
 }
 
-export function createTweet(req, res) {
+export async function createTweet(req, res) {
   const { text, username, name } = req.body;
-  const tweet = tweetRepository.create(text, username, name);
+  const tweet = await tweetRepository.create(text, username, name);
   res.status(201).json(tweet);
 }
 
-export function updateTweet(req, res) {
+export async function updateTweet(req, res) {
   const { id } = req.params;
   const { text } = req.body;
-  const tweet = tweetRepository.update(id, text);
+  const tweet = await tweetRepository.update(id, text);
   if (tweet) {
     res.status(200).json(tweet);
   } else {
@@ -35,8 +35,8 @@ export function updateTweet(req, res) {
   }
 }
 
-export function deleteTweet(req, res) {
+export async function deleteTweet(req, res) {
   const { id } = req.params;
-  tweetRepository.remove(id);
+  await tweetRepository.remove(id);
   res.sendStatus(204);
 }
