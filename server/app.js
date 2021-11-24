39dd81d7abac63e2c470d9a0795a7addc8261790
Yongveloper <aaa9015@naver.com>
2021-11-24 22:41:26 +0900

    배포 전 수정사항

--- server/app.js
+++ server/app.js
@@ -11,10 +11,13 @@ import { sequelize } from './db/database.js';
 
 const app = express();
 
+const corsOption = {
+  origin: config.cors.allowedOrigin,
+  optionsSuccessStatus: 200,
+};
 app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
+app.use(cors(corsOption));
 app.use(morgan('tiny'));
 
 app.use('/tweets', tweetsRouter);
@@ -30,8 +33,8 @@ app.use((error, req, res, next) => {
 });
 
 sequelize.sync().then(() => {
-  const server = app.listen(config.host.port, () =>
-    console.log(`Server listening on port:${config.host.port}`)
+  const server = app.listen(config.port, () =>
+    console.log(`Server listening on port:${config.port}`)
   );
   initSocket(server);
 });
